b5fa0412f26487693296f71d1a72cc5585171ade
Alfredo Pereda <aperar1500@alumno.ipn.mx>
2022-04-20 19:56:12 -0500

    Instacie la clase missionCommander y agregue npm start

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const Alfredo = new MissionCommander("Alfredo")
+const Alfred = new MissionCommander("Alfred")
+const Fello = new MissionCommander("Fello")
+console.log(Alfredo.name);
+console.log(Alfred.name);
+console.log(Fello.name);
