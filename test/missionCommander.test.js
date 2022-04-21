2074efb8bf4c647ff86dba4a05b70f77cdf63159
Alfredo Pereda <aperar1500@alumno.ipn.mx>
2022-04-21 02:30:48 -0500

    I did the Unit Test about missionCommander Class

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,15 @@
+const  MissionCommander = require('./../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1',() => {
         const result = 1 + 2
         expect(result).toBe(3);
     });
+})
+
+describe("Unit Tests for Mission Commander Class", () => {
+    test('Create a mission commander object', () => {
+        const mymissionCommander = new MissionCommander("Woopa")
+        expect(mymissionCommander.name).toBe("Woopa");
+    })
 })
